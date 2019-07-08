ea63198aca2cbc0b8a62a84999c6f381a2ca8c4f
rafael-vitor <rafael.vitor@jusbrasil.com.br>
2019-07-07 22:59:59 -0300

    Initial word highlighting

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import "./App.css";
 
 import ContentEditable from "react-contenteditable";
 
+const data = ["ravi", "gene", "dart", "murtinha"];
+
+const trimSpaces = string => {
+  return string
+    .replace(/&nbsp;/g, "")
+    .replace(/&amp;/g, "&")
+    .replace(/&gt;/g, ">")
+    .replace(/&lt;/g, "<");
+};
+
 class App extends React.Component {
   constructor() {
     super();
@@ -13,8 +23,21 @@ class App extends React.Component {
 
   handleContentEditable = e => {
     console.log("handle content editable change", e);
+    const { value } = e.target;
+    const stripedHtml = value.replace(/<[^>]+>/g, "");
+
+    const splitValue = stripedHtml.split(" ");
+
+    const formattedValue = splitValue
+      .map(word =>
+        word.match(/@\w/) && data.includes(trimSpaces(word).replace("@", ""))
+          ? `<span class="highlighted">${word}</span>`
+          : word
+      )
+      .join(" ");
+
     this.setState({
-      value: e.target.value
+      value: formattedValue
     });
   };
 
@@ -26,17 +49,17 @@ class App extends React.Component {
   };
 
   pasteAsPlainText = event => {
-    event.preventDefault()
-  
-    const text = event.clipboardData.getData('text/plain')
-    document.execCommand('insertHTML', false, text)
-  }
+    event.preventDefault();
+
+    const text = event.clipboardData.getData("text/plain");
+    document.execCommand("insertHTML", false, text);
+  };
 
   highlightAll = () => {
     setTimeout(() => {
-      document.execCommand('selectAll', false, null)
-    }, 0)
-  }
+      document.execCommand("selectAll", false, null);
+    }, 0);
+  };
 
   render() {
     const { value } = this.state;
@@ -44,13 +67,17 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="stage">
-          <input className="form-control" value={value} onChange={this.handleInputChange} />
+          <input
+            className="form-control"
+            value={value}
+            onChange={this.handleInputChange}
+          />
 
           <ContentEditable
             html={value}
             className="mention-input form-control"
             onChange={this.handleContentEditable}
-            onPaste={this.pasteAsPlainText} 
+            onPaste={this.pasteAsPlainText}
             onFocus={this.highlightAll}
           />
 
